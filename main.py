c10309e69e17a839322e11fa029e73b0104627b2
James-Muralles <james.muralles23@gmail.com>
2021-12-20 15:34:15 -0600

    practice using beatiful soup

--- main.py
+++ main.py
@@ -449,11 +449,28 @@ for topping in toppings:
 
 #%%
 
+import numpy
 
-     
+firstarray = numpy.empty([4,2], dtype = numpy.uint16)
+print("Printing Array")
+print(firstarray)
+print("Array shape is:",firstarray.shape)
+print("Dimensions:", firstarray.itemsize)
 
 
+#%%
 
+from bs4 import BeautifulSoup
+from requests import get
+url = 'https://www.imdb.com/search/title?release_date=2017&sort=num_votes,desc&page=1'
+response = get(url)
+print(response.text[:500])
+html_soup = BeautifulSoup(response.text, 'html.parser')
+type(html_soup)
+
+movie_containers = html_soup.find_all('div', class_ = 'lister-item mode-advanced')
+print(type(movie_containers))
+print(len(movie_containers))
 
 
 
